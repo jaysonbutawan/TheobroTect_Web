d6f09398c73ecab540e193a258f7eb810525f681
jaysonbutawan <159876164+jaysonbutawan@users.noreply.github.com>
2026-07-22 21:36:45 +0800

    Production Work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'NG_APP_API_URL',
+  apiUrl: 'https://theobrotect.org/api/theobrotect',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:3000/api/theobrotect',
-//   enableDebugTools: true,
-//   logLevel: 'debug'
-// };
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api/theobrotect',
+  enableDebugTools: true,
+  logLevel: 'debug'
+};
